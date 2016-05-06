0fdcac316753447d1ea396e6b0c8803ea2194ef2
Mark Siebert <mark@lucidchart.com>
2016-05-05 21:32:07 -0600

    type check jsx file

--- app/client/master/HelloWorld.jsx
+++ app/client/master/HelloWorld.jsx
@@ -1,10 +1,8 @@
+// @flow
+
 import React, {Component} from 'react'
 
 export default class HelloWorld extends Component {
-  constructor(props) {
-    super(props)
-  }
-
   render() {
     return (
       <h1>Hello World</h1>
